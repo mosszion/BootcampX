343957dfbcf4bbc0cba0f3fb39d1d43635a994e1
Mussie <musdzion@gmail.com>
2024-05-14 18:50:48 -0400

    add protection for sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,23 +7,24 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool 
-  .query (  `SELECT cohorts.name as name , teachers.name AS teacher
-  FROM assistance_requests
-  JOIN teachers ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
-  GROUP BY cohorts.name,teachers.name;
-  `)
-  
-  
+const values=[`${process.argv[2]}`]
+const queryString =  `SELECT cohorts.name as name , teachers.name AS teacher
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+GROUP BY cohorts.name,teachers.name;
+`
+
+pool
+  .query(queryString,values)
+
   .then((res) => {
     res.rows.forEach((user) => {
-      
       console.log(`${user.name}: ${user.teacher}`);
     });
-
-
   })
-  .catch((error) => { console.error("query error:", error.stack)});
+  .catch((error) => {
+    console.error("query error:", error.stack);
+  });
